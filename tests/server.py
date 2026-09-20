bc24d0d64c10c8c4a6fcb37a6ce85d63a244629d
Shane Jaroch <chown_tee@proton.me>
2026-09-20 15:50:02 -0400

    fix(test): preserve weakref to hs in shutdown_hs_on_cleanup for clean shutdown gc test

--- tests/server.py
+++ tests/server.py
@@ -2073,9 +2073,16 @@ def setup_test_homeserver(
         reactor=reactor,
     )
 
+    # Capture the `hs` as a `weakref` here to ensure there is no scenario where uncalled
+    # cleanup functions result in holding the `hs` in memory.
+    cleanup_hs_ref = weakref.ref(hs)
+
     def shutdown_hs_on_cleanup() -> "Deferred[None]":
+        cleanup_hs = cleanup_hs_ref()
+        if cleanup_hs is None:
+            return defer.succeed(None)
         _sd0 = time.monotonic()
-        deferred = defer.ensureDeferred(hs.shutdown())
+        deferred = defer.ensureDeferred(cleanup_hs.shutdown())
         if USE_POSTGRES_FOR_TESTS:
 
             def _record_shutdown_timing(result: Any) -> Any:
